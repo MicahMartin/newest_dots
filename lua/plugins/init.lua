641dd7fdab92e899e65a24820e87ac47e13c226c
MicahMartin <micah.m329@gmail.com>
2025-06-02 06:10:27 -0400

    we were sleepy

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,35 +1,7 @@
 M = {
-	{ "catgoose/nvim-colorizer.lua", event = "BufReadPre", opts = {}, },
-	{ "folke/which-key.nvim", event = "VeryLazy", opts = {}, },
-  { "neovim/nvim-lspconfig", lazy = false },
-  {
-    "mason-org/mason.nvim",
-    lazy = false,
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
-    }
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    lazy = false,
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "mason-org/mason.nvim"
-    },
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "html",
-        "cssls",
-      }
-    }
-  }
+	{ "catgoose/nvim-colorizer.lua", event = "BufReadPre", opts = {} },
+	{ "folke/which-key.nvim", event = "VeryLazy", opts = {} },
+	{ "neovim/nvim-lspconfig", lazy = false },
 }
 
 return M
