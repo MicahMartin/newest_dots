2cbb9948eb0120104af1166f9646eba300d178ad
MicahMartin <micah.m329@gmail.com>
2025-06-02 06:04:33 -0400

    ok nice

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,27 +1,30 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
-    }
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "mason-org/mason.nvim"
-    },
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "html",
-      }
-    }
-  }
+	{
+		"mason-org/mason.nvim",
+		lazy = false,
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		lazy = false,
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mason-org/mason.nvim",
+		},
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"html",
+				"pyrefly",
+			},
+		},
+	},
 }
