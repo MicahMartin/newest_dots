ff3077d293702910ee907485295a5312dd5ba286
MicahMartin <micah.m329@gmail.com>
2025-06-02 22:31:50 -0400

    just adding some mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -15,7 +15,11 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+
+vim.keymap.set("n", "<leader>wu", "<cmd>vertical resize +6 <cr>", { desc = "Quick horizontal increase" })
+vim.keymap.set("n", "<leader>wU", "<cmd>vertical resize -6 <cr>", { desc = "Quick horizontal decrease " })
+vim.keymap.set("n", "<leader>wd", "<cmd> resize +6 <cr>", { desc = "Quick vert increase" })
+vim.keymap.set("n", "<leader>wD", "<cmd> resize -6 <cr>", { desc = "Quick vert decrease" })
 
 vim.keymap.set("n", ",", "'", { desc = "Remap of mark jump prefix" })
 
@@ -23,7 +27,69 @@ vim.keymap.set("n", ",", "'", { desc = "Remap of mark jump prefix" })
 vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
 vim.keymap.set("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+-- Resizes
+vim.api.nvim_create_user_command("Vr", function()
+	local usage = "Usage: :Vr {number (1–100)}"
 
+	vim.ui.input({ prompt = "Vertical resize (%): " }, function(input)
+		-- If the user canceled or submitted an empty string, show usage and return
+		if not input or input == "" then
+			vim.notify(usage, vim.log.levels.WARN)
+			return
+		end
+
+		local pct = tonumber(input)
+		if not pct then
+			vim.notify("Not a valid number: " .. input, vim.log.levels.ERROR)
+			return
+		end
+
+		if pct <= 0 or pct > 100 then
+			vim.notify("Percentage must be between 1 and 100", vim.log.levels.ERROR)
+			return
+		end
+
+		-- Compute new width as total columns * (pct / 100)
+		local total_cols = vim.opt.columns:get()
+		local new_width = math.floor(total_cols * (pct / 100.0))
+		vim.notify_once("new width: " .. new_width)
+
+		vim.cmd("vertical resize " .. new_width)
+	end)
+end, { nargs = "?" })
+
+vim.api.nvim_create_user_command("Hr", function()
+	local usage = "Usage: :Hr {number (1–100)}"
+
+	vim.ui.input({ prompt = "horizontal resize (%): " }, function(input)
+		-- If the user canceled or submitted an empty string, show usage and return
+		if not input or input == "" then
+			vim.notify(usage, vim.log.levels.WARN)
+			return
+		end
+
+		local pct = tonumber(input)
+		if not pct then
+			vim.notify("Not a valid number: " .. input, vim.log.levels.ERROR)
+			return
+		end
+
+		if pct <= 0 or pct > 100 then
+			vim.notify("Percentage must be between 1 and 100", vim.log.levels.ERROR)
+			return
+		end
+
+		-- Compute new height as total rows * (pct / 100)
+		local total_rows = vim.opt.lines:get() - vim.opt.cmdheight:get()
+		local new_height = math.floor(total_rows * (pct / 100.0))
+		vim.notify_once("new height: " .. new_height)
+
+		vim.cmd("resize " .. new_height)
+	end)
+end, { nargs = "?" })
+
+vim.keymap.set("n", "<leader>w.", "<cmd>Hr<cr>", { desc = "Resize horizontal percent" })
+vim.keymap.set("n", "<leader>w,", "<cmd>Vr<cr>", { desc = "Resize vert percent" })
 -- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 --
 -- Dont deselect after indenting
