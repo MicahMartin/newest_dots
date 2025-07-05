57e855eb1076f1e4c59c1fc5505f30f195c76ff4
MicahMartin <micah.m329@gmail.com>
2025-07-05 18:15:18 -0400

    bruh

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,4 @@
 return {
-  -- including this inside treesittter
-  -- { "OXY2DEV/markview.nvim", lazy = false },
   { "catgoose/nvim-colorizer.lua", event = "BufReadPre", opts = {} },
   { "folke/which-key.nvim", event = "BufReadPre", opts = {} },
-  -- { "rafamadriz/friendly-snippets", event = "BufReadPre" },
 }
