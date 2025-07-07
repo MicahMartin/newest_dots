d0079430941b18584a163055a5c5660af1d4171c
MicahMartin <micah.m329@gmail.com>
2025-07-07 10:35:15 -0400

    we were actually sleep walking lol

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,5 +5,17 @@ return {
   config = function()
     require("mini.surround").setup({})
     require("mini.pairs").setup({})
+    require("mini.ai").setup({
+      mappings = {
+        around = "a",
+        inside = "i",
+        outside = "u",
+        delete = "d",
+        find = "f",
+        find_left = "F",
+        jump = "j",
+        last = "p",
+      },
+    })
   end,
 }
